wroyca/rounded-window-corners

commit f620bd238876037833195182c1d7a199fddc310f

fix: don't crash when opening settings
TypeScript 5 started generating properties from type annotations for
internal widget children, and those properties were overriding the ones
set by GTK, which lead to a crash. This is fixed by using the TS
`declare` keyword, which omits the fields from the generated JS code.

Fixes: #3

src/preferences/pages/blacklist.ts
@@ -24,8 +24,8 @@ export const BlackList = GObject.registerClass(
         InternalChildren: ['black_list_group', 'add_row_btn'],
     },
     class extends Gtk.Box {
-        private _black_list_group!: Gtk.ListBox;
-        private _add_row_btn!: Gtk.Button;
+        private declare _black_list_group: Gtk.ListBox;
+        private declare _add_row_btn: Gtk.Button;
 
         /** Store value of settings */
         private black_list!: string[];

src/preferences/pages/custom.ts
@@ -26,8 +26,8 @@ export const Custom = GObject.registerClass(
         InternalChildren: ['custom_group', 'add_row_btn'],
     },
     class extends Gtk.Box {
-        private _custom_group!: Gtk.ListBox;
-        private _add_row_btn!: Gtk.Button;
+        private declare _custom_group: Gtk.ListBox;
+        private declare _add_row_btn: Gtk.Button;
 
         private _settings_cfg!: CustomRoundedCornersCfg;
 

src/preferences/widgets/app_row.ts
@@ -30,15 +30,15 @@ export const AppRow = GObject.registerClass(
         ],
     },
     class extends Gtk.ListBoxRow {
-        private _wm_class_instance_entry!: Gtk.Entry;
-        private _remove_button!: Gtk.Button;
-        private _change_title_btn!: Gtk.Button;
-        private _pick_window_btn!: Gtk.Button;
-        private _title!: Gtk.Label;
-        private _description!: Gtk.Label;
-        private _expand_img!: Gtk.Button;
-        private _revealer!: Gtk.Revealer;
-        _expanded_list_box!: Gtk.ListBox;
+        private declare _wm_class_instance_entry: Gtk.Entry;
+        private declare _remove_button: Gtk.Button;
+        private declare _change_title_btn: Gtk.Button;
+        private declare _pick_window_btn: Gtk.Button;
+        private declare _title: Gtk.Label;
+        private declare _description: Gtk.Label;
+        private declare _expand_img: Gtk.Button;
+        private declare _revealer: Gtk.Revealer;
+        declare _expanded_list_box: Gtk.ListBox;
 
         private bind_property_handler?: GObject.Binding;
 

src/preferences/pages/general.ts
@@ -39,18 +39,17 @@ export const General = GObject.registerClass(
         ],
     },
     class extends Gtk.Box {
-        // Those properties come from 'InternalChildren'
-        private _global_settings_preferences_group!: Gtk.ListBox;
-        private _enable_log_switch!: Gtk.Switch;
-        private _skip_libhandy_app_switch!: Gtk.Switch;
-        private _skip_libadwaita_app_switch!: Gtk.Switch;
-        private _tweak_kitty_switch!: Gtk.Switch;
-        private _preferences_entry_switch!: Gtk.Switch;
-        private _border_width_ajustment!: Gtk.Adjustment;
-        private _border_color_button!: Gtk.ColorButton;
-        private _edit_shadow_row!: Gtk.ListBoxRow;
-        private _applications_group!: Gtk.ListBox;
-        private _reset_preferences_btn!: Gtk.Button;
+        private declare _global_settings_preferences_group: Gtk.ListBox;
+        private declare _enable_log_switch: Gtk.Switch;
+        private declare _skip_libhandy_app_switch: Gtk.Switch;
+        private declare _skip_libadwaita_app_switch: Gtk.Switch;
+        private declare _tweak_kitty_switch: Gtk.Switch;
+        private declare _preferences_entry_switch: Gtk.Switch;
+        private declare _border_width_ajustment: Gtk.Adjustment;
+        private declare _border_color_button: Gtk.ColorButton;
+        private declare _edit_shadow_row: Gtk.ListBoxRow;
+        private declare _applications_group: Gtk.ListBox;
+        private declare _reset_preferences_btn: Gtk.Button;
 
         private config_items!: _Items;
 

src/preferences/widgets/edit_shadow_window.ts
@@ -36,15 +36,15 @@ export const EditShadowWindow = GObject.registerClass(
         ],
     },
     class extends Gtk.Window {
-        private _opacity_scale!: Gtk.Scale;
-        private _spread_radius_scale!: Gtk.Scale;
-        private _blur_offset_scale!: Gtk.Scale;
-        private _vertical_offset_scale!: Gtk.Scale;
-        private _horizontal_offset_scale!: Gtk.Scale;
-        private _unfocus_shadow_widget!: Gtk.Widget;
-        private _focus_shadow_widget!: Gtk.Widget;
-        private _focus_toggle_button!: Gtk.ToggleButton;
-        private _unfocus_toggle_button!: Gtk.ToggleButton;
+        private declare _opacity_scale: Gtk.Scale;
+        private declare _spread_radius_scale: Gtk.Scale;
+        private declare _blur_offset_scale: Gtk.Scale;
+        private declare _vertical_offset_scale: Gtk.Scale;
+        private declare _horizontal_offset_scale: Gtk.Scale;
+        private declare _unfocus_shadow_widget: Gtk.Widget;
+        private declare _focus_shadow_widget: Gtk.Widget;
+        private declare _focus_toggle_button: Gtk.ToggleButton;
+        private declare _unfocus_toggle_button: Gtk.ToggleButton;
 
         // CssProvider to change style of preview widgets in edit window
         private unfocus_provider!: Gtk.CssProvider;

src/preferences/widgets/rounded_corners_item.ts
@@ -29,21 +29,21 @@ export const RoundedCornersItem = GObject.registerClass(
         ],
     },
     class extends Gtk.ListBox {
-        private _rounded_in_maximized_switch!: Gtk.Switch;
-        private _rounded_in_fullscreen_switch!: Gtk.Switch;
-        private _border_radius_scale!: Gtk.Scale;
-        private _smoothing_scale!: Gtk.Scale;
-        private _padding_left_scale!: Gtk.Scale;
-        private _padding_right_scale!: Gtk.Scale;
-        private _padding_top_scale!: Gtk.Scale;
-        private _padding_bottom_scale!: Gtk.Scale;
-        private _revealer!: Gtk.Revealer;
-        private _expander_img!: Gtk.Image;
+        private declare _rounded_in_maximized_switch: Gtk.Switch;
+        private declare _rounded_in_fullscreen_switch: Gtk.Switch;
+        private declare _border_radius_scale: Gtk.Scale;
+        private declare _smoothing_scale: Gtk.Scale;
+        private declare _padding_left_scale: Gtk.Scale;
+        private declare _padding_right_scale: Gtk.Scale;
+        private declare _padding_top_scale: Gtk.Scale;
+        private declare _padding_bottom_scale: Gtk.Scale;
+        private declare _revealer: Gtk.Revealer;
+        private declare _expander_img: Gtk.Image;
 
-        _paddings_row!: Gtk.ListBoxRow;
+        declare _paddings_row: Gtk.ListBoxRow;
 
-        private _scales!: Gtk.Scale[];
-        private _switches!: Gtk.Switch[];
+        private declare _scales: Gtk.Scale[];
+        private declare _switches: Gtk.Switch[];
 
         _init() {
             super._init();

commit 6d0504de727bf590d202f1a99a648b9e89dd692d

fix: open the window picker row properly
There were some variables that I missed in f620bd2 (fix: don't crash
when opening settings), which caused the window picker rows in "Custom"
and "Blacklist" pages to not open properly. This adds the missing
`declared` keywords to those variables to fix the issue.

Fixes: #5

src/preferences/pages/blacklist.ts
@@ -28,7 +28,7 @@ export const BlackList = GObject.registerClass(
         private declare _add_row_btn: Gtk.Button;
 
         /** Store value of settings */
-        private black_list!: string[];
+        private declare black_list: string[];
 
         _init() {
             super._init();

src/preferences/pages/custom.ts
@@ -29,7 +29,7 @@ export const Custom = GObject.registerClass(
         private declare _custom_group: Gtk.ListBox;
         private declare _add_row_btn: Gtk.Button;
 
-        private _settings_cfg!: CustomRoundedCornersCfg;
+        private declare _settings_cfg: CustomRoundedCornersCfg;
 
         _init() {
             super._init();

src/preferences/widgets/app_row.ts
@@ -40,7 +40,7 @@ export const AppRow = GObject.registerClass(
         private declare _revealer: Gtk.Revealer;
         declare _expanded_list_box: Gtk.ListBox;
 
-        private bind_property_handler?: GObject.Binding;
+        private declare bind_property_handler?: GObject.Binding;
 
         /** Store event handlers for this widget */
         private cb?: AppRowHandler;