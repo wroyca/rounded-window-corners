6453e07809a8805ca94765d4419daa55555b4595
flexagoon <code@fxgn.dev>
2024-07-18 20:50:00 -0700

    fix: make windows in overview slightly less pixelated
    Using a linear filter instead of nearest neighbor for texture
    magnification makes the windows look better, which makes the side effect
    of 1c0989c much less significant.

--- src/effect/linear_filter_effect.ts
+++ src/effect/linear_filter_effect.ts
@@ -17,7 +17,7 @@ export const LinearFilterEffect = GObject.registerClass(
             this.get_pipeline()?.set_layer_filters(
                 0,
                 Cogl.PipelineFilter.LINEAR_MIPMAP_LINEAR,
-                Cogl.PipelineFilter.NEAREST,
+                Cogl.PipelineFilter.LINEAR,
             );
             super.vfunc_paint_target(node, ctx);
         }
